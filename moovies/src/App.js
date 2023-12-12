09544fa22e5993dca807aa0b682463fca843d947
Miruo Lin <miruomoo@gmail.com>
2023-12-11 19:38:33 -0500

    Added delete function

--- moovies/src/App.js
+++ moovies/src/App.js
@@ -1,53 +1,6 @@
 import { useEffect, useState } from "react";
 import StarRating from "./starRating";
 
-const tempMovieData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt0133093",
-    Title: "The Matrix",
-    Year: "1999",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-  },
-  {
-    imdbID: "tt6751668",
-    Title: "Parasite",
-    Year: "2019",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
@@ -122,7 +75,7 @@ function Box({children}){
 </div>)
 }
 
-function WatchedMovie({movie}){
+function WatchedMovie({movie, onDelete}){
   return (    <li key={movie.imdbID}>
     <img src={movie.poster} alt={`${movie.title} poster`} />
     <h3>{movie.title}</h3>
@@ -139,15 +92,16 @@ function WatchedMovie({movie}){
         <span>⏳</span>
         <span>{movie.runtime} min</span>
       </p>
+      <button class="btn-delete" onClick={() => onDelete(movie.imdbID)}>X</button>
     </div>
   </li>)
 }
 
-function WatchedMoviesList({watched}){
+function WatchedMoviesList({watched, onDelete}){
   return (
   <ul className="list">
   {watched.map((movie) => (
-    <WatchedMovie movie={movie} key={movie.imdbID}></WatchedMovie>
+    <WatchedMovie movie={movie} key={movie.imdbID} onDelete={onDelete}></WatchedMovie>
   ))}
 </ul>)
 }
@@ -166,11 +120,11 @@ function WatchedSummary({watched}){
     </p>
     <p>
       <span>🌟</span>
-      <span>{avgImdbRating}</span>
+      <span>{avgImdbRating.toFixed(2)}</span>
     </p>
     <p>
       <span>⭐️</span>
-      <span>{avgUserRating}</span>
+      <span>{avgUserRating.toFixed(2)}</span>
     </p>
     <p>
       <span>⏳</span>
@@ -264,7 +218,7 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, addedMovie}){
 export default function App() {
   const [query, setQuery] = useState("jujutsu kaisen");
   const [watched, setWatched] = useState([]);
-  const [movies, setMovies] = useState(tempMovieData);
+  const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
   const [selectedId, setSelectedId] = useState(null);
@@ -283,6 +237,12 @@ export default function App() {
     }else{
       setIsWatched(null)
     }
+  }
+
+  function handleDeleteWatched(id){
+    setWatched(watched => watched.filter((movie) => 
+      movie.imdbID !== id
+    ))
   }
 
   function handleCloseMovie(){
@@ -338,7 +298,7 @@ export default function App() {
            selectedId={selectedId} onCloseMovie={handleCloseMovie} onAddWatched={handleAddWatched} addedMovie={isWatched}/>:
           <>
           <WatchedSummary watched={watched}></WatchedSummary>
-          <WatchedMoviesList watched={watched}></WatchedMoviesList>
+          <WatchedMoviesList watched={watched} onDelete={handleDeleteWatched}></WatchedMoviesList>
           </>
           }
         </Box>
